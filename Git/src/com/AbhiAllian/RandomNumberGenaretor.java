4f559f69f6bd3400e8a0f3d7901f9ba6e02f67a2
Abhishek <abhisand100@gmail.com>
2017-08-13 14:36:50 +0530

    Random Number Genaretor Class

---
+++ Git/src/com/AbhiAllian/RandomNumberGenaretor.java
@@ -0,0 +1,13 @@
+package com.AbhiAllian;
+
+import java.util.Random;
+
+public class RandomNumberGenaretor {
+
+	public static void main(String[] args) {
+		Random r= new Random();
+		System.out.println("Random No:"+r.nextInt(10));
+
+	}
+
+}
